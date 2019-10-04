08fa046dbd0fef0c6c6ac6b2a0b0a0ed27e47d65
sangar3 <55110746+sangar3@users.noreply.github.com>
2019-10-04 17:24:23 -0500

    step 7

---
+++ Assignment 6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * (Santiago Garcia II  )
+ * (Player Controller.cs)
+ * (Assignment 6 practice )
+ * (Gives player movement )
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed );
+    }
+}
